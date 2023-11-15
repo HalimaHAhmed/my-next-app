1bb731a28f04616d4b21512a8724192cc4ed45dc
HalimaHAhmed <hanahussein6155@gmail.com>
2023-11-15 16:21:57 +0300

    users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const Userpage = () => {
-  return (
-    <div>Userpage</div>
-  )
+interface User {
+  id: number;
+  name: string;
 }
 
-export default Userpage
+const UsersPage = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data: User[] = await res.json();
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []); // Empty dependency array ensures useEffect runs only once after initial render
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
